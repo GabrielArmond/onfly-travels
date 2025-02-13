73df634ed26583386ab8317ab9d0c164d03df342
Gabriel Guerra <gabriel.armond77@gmail.com>
2025-02-13 18:12:32 -0300

    feat: init project

---
+++ src/components/BookHotel.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-card class="my-card text-black" style="width: 100%;">
+    <q-card-section>
+      <div class="text-h6">Reservar hotel</div>
+    </q-card-section>
+
+    <q-separator size="5px" />
+
+    <q-card-actions vertical>
+      <q-btn flat>Action 1</q-btn>
+      <q-btn flat>Action 2</q-btn>
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped></style>
