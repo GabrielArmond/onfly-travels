c9f90df2d934b9c5d4da58b5f41868f8e299912b
Gabriel Guerra <gabriel.armond77@gmail.com>
2025-02-19 13:11:00 -0300

    fix: build and add speed Insights

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <router-view class="bg-grey-2" />
+  <SpeedInsights />
 </template>
 
 <script setup lang="ts">
-//
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
